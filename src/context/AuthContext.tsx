aa1c2bb4bf562702d7348d57fac8c05cc98f7ee9
ahmadou5 <ahmadlasauwal@gmail.com>
2024-09-19 18:05:13 +0100

    start: new2473

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -37,12 +37,7 @@ export default function AuthContextProvider({children}: ReactChildrenProps) {
       if (savedUser) {
         handleSetUser(savedUser)
       } else if (tgData?.user?.id) {
-        setTimeout(() => {
-          if(tgData.user?.id)
-            fetchProfile(tgData.user.id)
-            console.log('Function triggered after 20 seconds!');
-        }, 20000);
-        
+        await fetchProfile(tgData.user.id)
       } else {
         setIsUserLoading(false)
       }
