9b34e6ae0ccb0261ecc2a8cf438a71c2333ea56f
ahmadou5 <ahmadlasauwal@gmail.com>
2024-09-19 19:15:44 +0100

    start: new24743

--- src/views/WalletView.tsx
+++ src/views/WalletView.tsx
@@ -5,7 +5,7 @@ interface token {
   [];
   
   
-  interface TeamList extends Array<token> {}
+interface TeamList extends Array<token> {}
 export const WalletView = ( ) => {
     const token1:TeamList =[
         {
@@ -18,13 +18,13 @@ export const WalletView = ( ) => {
         <div className="bg-gothic-950/0 mt-0.5 flex  mb-2 flex-col items-center justify-center w-[100%] h-auto">
           <div className="mt-0.5 mb-4 w-full flex">
            <div className="bg-white/15 border border-[#448cff]/45 text-white mt-1 rounded-xl p-1.5 flex ml-auto mr-[45px] w-[51%] h-9">
-           <img src='./assets/5426.png' className="mr-1 w-6 h-6 rounded-full"/>
+           <img src='https://solana-wallet-orcin.vercel.app/assets/5426.png' className="mr-1 w-6 h-6 rounded-full"/>
             <div className="mb-0.5">{'SOLANA'}</div>
             <MdKeyboardArrowDown className="text-2xl text-[#448cff]/45 ml-auto mr-1 mb-2" />
            </div>
            <div  className="w-11 p-2 mr-1.5 flex items-center justify-center rounded-full">
              
-              <img src="./assets/setting.svg" className="text-white" />
+              <img src="https://solana-wallet-orcin.vercel.app/assets/setting.svg" className="text-white" />
            </div>
           </div>
           <div className="bg-s-gray-300/0 w-[90%] flex flex-col items-center justify-center rounded-3xl h-[120px]">
@@ -39,14 +39,14 @@ export const WalletView = ( ) => {
             
               className="text-xl bg-white/10  border-[#448cff]/25 flex flex-col items-center justify-center rounded-3xl h-20 w-20 ml-auto mr-auto  text-white/60"
             >
-              <img src="./assets/send.svg" className="mt-1" />
+              <img src="https://solana-wallet-orcin.vercel.app/assets/send.svg" className="mt-1" />
               <p className="text-sm mt-2.5 text-white/50 font-light ">Send</p>
             </div>
             <div
              
               className="text-3xl  bg-white/10 flex flex-col items-center justify-center rounded-3xl h-20 w-20 ml-auto mr-auto  text-white/60"
             >
-              <img src="./assets/receive.svg" className="mt-1" />
+              <img src="https://solana-wallet-orcin.vercel.app/assets/receive.svg" className="mt-1" />
               <p className="text-sm mt-2.5 text-white/50 font-light ">
                 Receive
               </p>
@@ -54,22 +54,13 @@ export const WalletView = ( ) => {
            
           </div>
         </div>
-        {/**<div className="bg-gothic-950/0 mt-16 flex items-center justify-center w-[100%] h-auto">
-          <div className="bg-gothic-300/5 w-[90%] flex items-center justify-center rounded-3xl h-[100px]">
-              <div className="bg-gothic-600/85 w-9 flex items-center justify-center h-9 ml-[40px] mr-[20px] rounded-full">
-                  <IoKey className="text-white/90 text-2xl" />
-              </div>
-              <div className="ml-[10px] mr-auto px-3">
-                  <p className="text-sm">BackUp Your Seed Phrases</p>
-              </div>
-          </div>
-  </div> **/}
+       
         <div className="bg-gothic-950/0 mt-8 flex flex-col items-center justify-center w-[100%] h-auto">
           <p className=" ml-7 mr-auto text-[16px] font-light text-[#ddd9d9] mb-1.5">Balances</p>
           <div className="bg-white/10 w-[90%] mb-1.5 flex items-center justify-center rounded-3xl h-[70px]">
             <div className="bg-gothic-600/85 w-12 flex items-center justify-center h-12 ml-[23px] mr-[10px] rounded-full">
               <img
-                src={"./assets/5426.png"}
+                src={"https://solana-wallet-orcin.vercel.app/assets/5426.png"}
                 className="text-white/90 w-full h-full rounded-full text-2xl"
               />
             </div>
@@ -92,7 +83,7 @@ export const WalletView = ( ) => {
               <div key={i} className="bg-white/10 w-[90%] mb-0.5 flex items-center justify-center rounded-3xl h-[70px]">
             <div className="bg-gothic-600/85 w-12 flex items-center justify-center h-12 ml-[23px] mr-[10px] rounded-full">
               <img
-                src={"./assets/5426.png"}
+                src={"https://solana-wallet-orcin.vercel.app/assets/5426.png"}
                 className="text-white/90 w-full h-full rounded-full text-2xl"
               />
             </div>
