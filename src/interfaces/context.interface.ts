9992e04740715c51c0286824e21f867aaffa89b6
ahmadou5 <ahmadlasauwal@gmail.com>
2024-10-11 17:17:36 +0100

    ...integer4

--- src/interfaces/context.interface.ts
+++ src/interfaces/context.interface.ts
@@ -1,14 +1,6 @@
 import { UserInterface } from '.'
 import { Dispatch, SetStateAction } from 'react';
 
-export interface TokenBalance {
-  [address: string]: number
-}
-
-
-export interface TokenPrices {
-  [ticker: string]: number;
-}
 export interface AuthContextProps {
   isLoggedIn: boolean;
   user: UserInterface | undefined;  // Allow undefined
@@ -19,18 +11,12 @@ export interface AuthContextProps {
 }
 
 export interface MiniContextType {
-  solBalance: number|undefined;
-  solPrice: number|undefined;
+  solBalance: number;
   isCompressed: boolean;
   isLoading: boolean;
-  tokenBalance: TokenBalance
-  tokenPrices: TokenPrices;
-  setTokenBalance: Dispatch<SetStateAction<TokenBalance>>
-  setTokenPrices: Dispatch<SetStateAction<TokenPrices>>
   setIsLoading: Dispatch<SetStateAction<boolean>>;
   setIsCompressed: Dispatch<SetStateAction<boolean>>;
   setSolBalance: Dispatch<SetStateAction<number>>;
-  setSolPrice: Dispatch<SetStateAction<number>>;
 }
 
 export interface AppContextProps {
