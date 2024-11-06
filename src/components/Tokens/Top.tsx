aa031dddf38da272beff70df61cdde4315078787
ahmadou5 <ahmadlasauwal@gmail.com>
2024-11-06 03:09:01 +0100

    mainnet1

--- src/components/Tokens/Top.tsx
+++ src/components/Tokens/Top.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/navigation";
-import { ArrowLeft } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
 import { useEffect, useState } from "react";
 import { TokenService } from "@/lib/services/TokenServices";
 import { Tokens } from "@/interfaces/models.interface";
@@ -16,7 +16,7 @@ interface TopProps {
 export const Top = ({ tokenId }: TopProps) => {
   const { user } = useAuth();
   const { network } = useNetwork();
-  const [userBalance, setUserBalance] = useState<number>();
+  const [userBalance, setUserBalance] = useState<number | null>(null);
   const [tokenInfo, setTokenInfo] = useState<Tokens[]>([]);
   const connection = new Connection(network.rpcUrl || clusterApiUrl("devnet"), {
     commitment: "confirmed",
@@ -96,19 +96,17 @@ export const Top = ({ tokenId }: TopProps) => {
   }, [user]);
   return (
     <div className="bg-white/0 w-[96%] mt-2 ml-auto mr-auto py-1 px-2 rounded-lg ">
-      <div className=" bg-slate-50/0  mb-[26px] w-[100%] flex  ">
-        <div
-          onClick={() => router.back()}
-          className="bg-white/5 flex items-center justify-center w-12 rounded-xl ml-0 h-11"
-        >
-          <ArrowLeft className="font-bold text-xl" />
-        </div>
-        <div className="ml-auto text-lg mt-2 mr-[41%]">
-          {tokenId[0] === network.native?.name.toLowerCase()
-            ? network.native.name
-            : tokenInfo[0]?.name}
+      <div className="flex items-center justify-between px-1 py-1">
+        <div onClick={() => router.back()} className="flex items-center">
+          <ChevronLeft className="w-6 h-6 mr-4" />
+          <h1 className="text-xl font-medium">
+            {tokenId[0] === network.native?.name.toLowerCase()
+              ? network.native.name
+              : tokenInfo[0]?.name}
+          </h1>
         </div>
       </div>
+
       <div className=" bg-slate-50/0 mb-[20px] py-2 px-2 h-[80px] w-[100%] flex  ">
         {tokenId[0] === network.native?.name.toLowerCase() ? (
           <div className="bg-white/5 flex items-center ml-7 justify-center w-[70px] rounded-full h-[70px]">
@@ -145,11 +143,7 @@ export const Top = ({ tokenId }: TopProps) => {
             <div className="flex">
               {" "}
               <p className="ml-2 text-4xl mr-2">{`${
-                userBalance === undefined ? (
-                  <div className="bg-white/20 h-4 w-16 mb-2 animate-pulse rounded-xl"></div>
-                ) : (
-                  SolConverter(userBalance).toFixed(2)
-                )
+                userBalance === null ? 0 : SolConverter(userBalance)?.toFixed(2)
               }`}</p>
               <p className="mt-3">{` ${
                 tokenId[0] === network.native?.name.toLowerCase()
@@ -165,11 +159,7 @@ export const Top = ({ tokenId }: TopProps) => {
                 <div className="flex">
                   {" "}
                   <p className="ml-2 text-4xl mr-2">{`${
-                    !userBalance ? (
-                      <div className="bg-white/20 h-4 w-16 mb-2 animate-pulse rounded-lg"></div>
-                    ) : (
-                      userBalance?.toFixed(2)
-                    )
+                    userBalance === null ? 0 : userBalance
                   }`}</p>
                   <p className="mt-[11px]">
                     {` ${
