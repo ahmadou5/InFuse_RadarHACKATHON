81d4048152b4ae6012334125725ed6a244813e9a
ahmadou5 <ahmadlasauwal@gmail.com>
2025-01-21 15:26:15 +0100

    all

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,10 +1,11 @@
-
-import { WalletView } from "@/views/WalletView"
+import { WalletView } from "@/views/WalletView";
 
 export default function Wallet() {
   return (
-  <div className="flex min-h-screen bg-[#448cff] bg-black/0 flex-col items-center justify-between p-1.5">
-     <WalletView/>
-  </div>
-)
+    <div
+      className={`flex min-h-screen bg-[#448cff] bg-black/0 flex-col items-center justify-between p-1.5`}
+    >
+      <WalletView />
+    </div>
+  );
 }
